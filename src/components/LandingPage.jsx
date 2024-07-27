c1b7a9b5e023082c6dcd9ef43092e74130bb21a2
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-27 22:46:05 +0530

    Started Working on landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+        <div className='textstructure'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
