7a762b78639aa06024e4c90a5265f96d2d3a3b71
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-28 22:04:05 +0530

    Made the masker

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div className='w-full h-screen bg-zinc-900'>
-        <div className='textstructure'>
-            
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div className='textstructure mt-52 px-20'>
+            {["We Create","Eye Opening","Presentations"].map((item,index)=>{
+                return <div className='masker'>
+                            <h1 className='uppercase text-[7.5vw] leading-[6vw] tracking-tighter  font-["Test Founders Grotesk X-Cond SmBd"] font-medium'>{item}</h1>           
+                        </div>
+            })}
         </div>
+        <div></div> 
     </div>
   )
 }
