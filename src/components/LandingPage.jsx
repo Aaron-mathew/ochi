900d4c130e16c0805b55fe206488b37801e1c124
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-29 00:25:46 +0530

    made the start the project part

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import { FaArrowUpLong } from "react-icons/fa6";
 
 const LandingPage = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 pt-1'>
         <div className='textstructure mt-52 px-20'>
-            {["We Create","Eye Opening","Presentations"].map((item,index)=>{
-                return <div className='masker'>
-                            <h1 className='uppercase text-[7.5vw] leading-[6vw] tracking-tighter  font-["Test Founders Grotesk X-Cond SmBd"] font-medium'>{item}</h1>           
-                        </div>
+            {["We Create","Eye-Opening","Presentations"].map((item,index)=>{
+                return  (<div className='masker bg-red-600'>
+                          <div className='w-fit flex'>
+                            {index === 1 && <div className='w-[9vw] h-[6vw] relative -top-[1vw] bg-red-500'>
+                              </div>}
+                            <h1 className='flex items-center uppercase text-[9vw] h-full leading-[7vw] font-["Founders Grotesk X-Condensed"] font-bold'>{item}</h1>                                    
+                          </div>
+                        </div>)
             })}
         </div>
-        <div></div> 
+        <div className='border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20'>
+          {["For public and private companies", "From the first pitch to IPO"].map((item,index)=><p className='text-md font-light tracking-tight leading-none'>{item}</p>  )}
+          <div className='start flex items-center gap-5'>
+            <div className='px-5 py-2 border-[1px] border-zinc-400 font-light text-md uppercase rounded-full'>start the project</div>
+            <div className='w-10 h-10 flex items-center justify-center border-[1px] border-zinc-400 rounded-full'>
+              <span className='rotate-[45deg]'>
+                <FaArrowUpLong />
+              </span>
+            </div>
+          </div>
+        </div> 
     </div>
   )
 }
 
 export default LandingPage
+
