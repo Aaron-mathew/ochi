fc415beb16831ffc1bc6512d5ce0a4008caa253e
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-30 23:10:08 +0530

    Fixed font problem and started working on marquee

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,16 +6,16 @@ const LandingPage = () => {
     <div className='w-full h-screen bg-zinc-900 pt-1'>
         <div className='textstructure mt-52 px-20'>
             {["We Create","Eye-Opening","Presentations"].map((item,index)=>{
-                return  (<div className='masker bg-red-600'>
-                          <div className='w-fit flex'>
-                            {index === 1 && <div className='w-[9vw] h-[6vw] relative -top-[1vw] bg-red-500'>
+                return  (<div className='masker'>
+                          <div className='w-fit flex items-end '>
+                            {index === 1 && <div className='mr-[1vw] w-[7vw] rounded-md h-[5vw] -top-[-1vw] relative bg-green-500'>
                               </div>}
-                            <h1 className='flex items-center uppercase text-[9vw] h-full leading-[7vw] font-["Founders Grotesk X-Condensed"] font-bold'>{item}</h1>                                    
+                            <h1 className='pt-[2vw] -mb-[1vw] uppercase text-[8vw] leading-[.75]'>{item}</h1>                                    
                           </div>
                         </div>)
             })}
         </div>
-        <div className='border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20'>
+        <div className='border-t-[1px] border-zinc-800 mt-20 flex justify-between items-center py-5 px-20'>
           {["For public and private companies", "From the first pitch to IPO"].map((item,index)=><p className='text-md font-light tracking-tight leading-none'>{item}</p>  )}
           <div className='start flex items-center gap-5'>
             <div className='px-5 py-2 border-[1px] border-zinc-400 font-light text-md uppercase rounded-full'>start the project</div>
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Marquee = () => {
+  return (
+    <div className='w-full h-[50vh] bg-[#004D43]'>
+        <div className='text'>
+            <h1>we are ochi</h1>
+            <h1>we are ochi</h1>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Marquee
