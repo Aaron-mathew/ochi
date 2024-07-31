cb23babbd8de777e64a383e69ab9a9b78d329a6a
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-31 23:47:07 +0530

    completed the marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,12 +1,12 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const Marquee = () => {
   return (
-    <div className='w-full h-[50vh] bg-[#004D43]'>
-        <div className='text'>
-            <h1>we are ochi</h1>
-            <h1>we are ochi</h1>
-
+    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+        <div className='text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap'>
+            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-10 font-semibold '>we are ochi</motion.h1>
+            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-10 font-semibold'>we are ochi</motion.h1>
         </div>
 
     </div>
