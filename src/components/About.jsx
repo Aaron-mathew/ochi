3be2f0301baa6861f1874b559418689191aa342e
Aaron Mathew <aaronmathew453@gmail.com>
2024-08-02 00:08:06 +0530

    Made some progress in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-full py-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl'>
-        
+    <div className='w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black'>
+      <h1 className='font-["Neue Montreal"] text-[3vw] leading-[3.3vw] tracking-tight'>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
+      <div className='w-full border-t-[1px] pt-20 mt-20 border-[#a1b562]'>
+        <div className='w-1/2'>
+        <h1 className='text-6xl'>Our Approach:</h1>
+        <button className='flex uppercase gap-10 items-center px-10 py-6 bg-zinc-900 mt-10 rounded-full text-white'>Read More
+          <div className='w-2 h-2 bg-zinc-100 rounded-full'></div>
+        </button>
+        </div>
+        <div></div>
+      </div>
     </div>
   )
 }
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,8 +5,8 @@ const Marquee = () => {
   return (
     <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
         <div className='text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap'>
-            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-10 font-semibold '>we are ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-10 font-semibold'>we are ochi</motion.h1>
+            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-[3vw] font-semibold '>we are ochi</motion.h1>
+            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-[3vw] font-semibold'>we are ochi</motion.h1>
         </div>
 
     </div>
