22bfc060d50f1c9113aa529b5d6cb2c0ae231b9f
Aaron Mathew <aaronmathew453@gmail.com>
2024-08-07 22:32:16 +0530

    Started working on Featured

---
+++ src/components/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Featured = () => {
+  return (
+    <div className='w-full py-10'>
+        <div className='w-full px-10'>
+            <h1 className='text-8xl'>Featured</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
+
