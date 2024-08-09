daecbb41f9db7124c8685105caaffd5f29b13b2a
Aaron Mathew <aaronmathew453@gmail.com>
2024-08-10 00:07:55 +0530

    Started working on Cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-100'>
+        <div className='cardcontainer'></div>
+    </div>
+  )
+}
+
+export default Cards
