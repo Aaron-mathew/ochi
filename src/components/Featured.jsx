50b0423d61a93783f104420a94918a313ac49cb0
Aaron Mathew <aaronmathew453@gmail.com>
2024-08-09 23:28:38 +0530

    Completed Featured

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -6,10 +6,24 @@ const Featured = () => {
         <div className='w-full px-20 border-b-[1px] border-zinc-700 pb-20'>
             <h1 className='text-8xl font-["Neue Montreal"] tracking-tight'>Featured projects</h1>
         </div>
-        <div>
+        <div className='px-20'>
           <div className="cards w-full flex gap-10 mt-10">
-                <div className='card w-1/2 h-[80vh] bg-red-500'></div>
-                <div className='card w-1/2 h-[80vh] bg-red-500'></div>
+                <div className='cardcontainer relative  w-1/2 h-[75vh]'>
+                  <h1 className='absolute text-[#CDEA68] left-full -translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
+                    {"FIDE".split('').map((item,index)=><span>{item}</span>)}
+                  </h1>
+                  <div className='card w-full h-full rounded-xl overflow-hidden'>
+                    <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-1326x1101.png" alt="" />
+                  </div>
+                </div>
+                <div className='cardcontainer relative rounded-xl w-1/2 h-[75vh]'>
+                  <h1 className='absolute text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
+                    {"VISE".split('').map((item,index)=><span>{item}</span>)}
+                  </h1>
+                  <div className='card w-full h-full rounded-xl overflow-hidden'>
+                    <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-1326x1101.jpg" alt="" />
+                  </div>
+                </div>
           </div>
         </div>
     </div>
