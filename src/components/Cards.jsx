ae7ff7b1003c9154b2a67cb8d92219d60fbf9949
Aaron Mathew <aaronmathew453@gmail.com>
2024-08-12 22:09:37 +0530

    Made some progress in Cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Cards = () => {
   return (
     <div className='w-full h-screen bg-zinc-100'>
-        <div className='cardcontainer'></div>
+        <div className='cardcontainer w-1/2'>
+          <div className='card w-full h-[50vh] bg-[#004D43]'></div>
+        </div>
+        <div className='cardcontainer w-1/2'></div>
     </div>
   )
 }
