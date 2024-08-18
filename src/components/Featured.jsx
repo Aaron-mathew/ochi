acb6726aea632af9a6b8610945a8134e468b2af2
Aaron Mathew <aaronmathew453@gmail.com>
2024-08-18 23:53:15 +0530

    Made some progress in animations

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { Power4 } from 'gsap';
+import React, { useState } from 'react'
 
 const Featured = () => {
+
   return (
     <div className='w-full py-20'>
         <div className='w-full px-20 border-b-[1px] border-zinc-700 pb-20'>
@@ -9,9 +12,11 @@ const Featured = () => {
         <div className='px-20'>
           <div className="cards w-full flex gap-10 mt-10">
                 <div className='cardcontainer relative  w-1/2 h-[75vh]'>
-                  <h1 className='absolute text-[#CDEA68] left-full -translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
+                <div className='absolute text-[#CDEA68] left-full -translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
+                  <h1 className='absolute flex overflow-hidden right-full translate-x-1/2 '>
                     {"FIDE".split('').map((item,index)=><span>{item}</span>)}
                   </h1>
+                </div>
                   <div className='card w-full h-full rounded-xl overflow-hidden'>
                     <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-1326x1101.png" alt="" />
                   </div>
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Marquee = () => {
   return (
-    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
-        <div className='text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap'>
-            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-[3vw] font-semibold '>we are ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-[3vw] font-semibold'>we are ochi</motion.h1>
+    <div data-scroll data-scroll-section data-scroll-speed="0.1" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+        <div className='text border-t-2 border-b-2 gap-10 border-zinc-300 flex overflow-hidden whitespace-nowrap'>
+            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-[3vw] font-semibold'>we are ochi</motion.h1>
+            <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10 }} className='text-[27vw] leading-none font-["FoundersGrotesk-Semibold"] uppercase -mt-[3vw] font-semibold'>we are ochi</motion.h1>
         </div>
 
     </div>
